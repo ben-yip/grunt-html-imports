61366127cb8569421540a0413ea657077c7af319
BenYip <endless_leaf@163.com>
2017-01-06 16:16:01 +0800

    Add jshint settings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,8 @@ module.exports = function (grunt) {
                 '<%= nodeunit.tests %>'
             ],
             options: {
-                jshintrc: '.jshintrc'
+                jshintrc: '.jshintrc',
+                reporterOutput: ''
             }
         },
 
